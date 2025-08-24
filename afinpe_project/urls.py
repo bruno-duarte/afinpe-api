60784bb00f1f93955833ad60a39e8845726c240d
Bruno Duarte <bruno.duarte@aluno.uece.br>
2025-08-24 12:22:32 -0300

    Chore: setting up application

--- afinpe_project/urls.py
+++ afinpe_project/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,4 +11,7 @@ urlpatterns = [
     path("api/v1/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     # App v1
     path("api/v1/", include("core.urls")),
+
+    # Redireciona a raiz ("/") para o Swagger
+    path("", RedirectView.as_view(url="/api/v1/schema/swagger-ui/", permanent=False)),
 ]
